c310c22b9fe017c4fb5b0bcf7e9f08bf44360b69
rko2 <rko2@users.noreply.github.com>
2016-09-06 12:17:15 -0500

    add knockout and hide account form upon submission

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+var viewModel = function () {
+  var self = this;
+  var accountForm = document.getElementById('account');
+  self.accountSubmit = function() {
+    accountForm.classList.add('invisible');
+  }
+}
+
+ko.applyBindings(new viewModel());
