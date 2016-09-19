6a6cc99ff24c97185aabc9e235b26e9e3f64efb6
rko2 <rko2@users.noreply.github.com>
2016-09-19 16:58:08 -0500

    added basic functionality for adding events

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,22 +7,40 @@ var viewModel = function () {
   var progressBar = document.getElementById('progress');
   var passFirst = document.getElementById('password');
   var passSecond = document.getElementById('confirmpass');
+  self.eventName = ko.observable('');
+  self.eventType = ko.observable('');
+  self.eventList = ko.observableArray([]);
   passSecond.addEventListener('input', function(e) {
     console.log(passSecond.value);
     if (passSecond.value != passFirst.value) {
-      passSecond.setCustomValidity("The passwords must match.");
+      passSecond.setCustomValidity('The passwords must match.');
     } else {
-      passSecond.setCustomValidity("");
+      passSecond.setCustomValidity('');
     }
   })
   self.accountSubmit = function() {
     progressBar.value = 50;
-    accountForm.innerHTML = "<h1>Thanks!</h1>";
-    eventForm.classList.remove("invisible");
+    accountForm.innerHTML = '<h1>Thanks!</h1>';
+    eventForm.classList.remove('invisible');
   }
   self.eventSubmit = function() {
-    console.log(test);
+    console.log(self.eventName());
+    addEvent();
+    self.eventName('');
+    self.eventType('');
+    console.log(self.eventName());
   }
+
+  var addEvent = function() {
+    self.eventList.push(new newEvent());
+    console.log(self.eventList());
+  }
+
+  var newEvent = function() {
+    this.name = self.eventName();
+    this.type = self.eventType();
+  }
+
 }
 
 ko.applyBindings(new viewModel());
